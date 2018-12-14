a0f07851efe81f8b43bd1c71ac9fd5bbcff13d8c
LeonardoCatelan <44733148+LeonardoCatelan@users.noreply.github.com>
2018-12-14 08:57:40 -0200

    redoing everything, callback errors done printing latitude and longitute

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
-const request = require('request');
-const yargs = require('yargs');
+const request = require ('request');
+const yargs = require ('yargs');
 
 const argv = yargs
 .options({
     a: {
         demand: true,
-        alias: 'address',
-        describe: 'Address to fetch weather for',
+        // alias = 'address',
+        describe: 'Address you wanna fetch the weather',
         string: true
     }
-}) 
-.help()
+})
+.   help()
 .alias('help', 'h')
 .argv;
 
 const addressInput = encodeURIComponent(argv.a);
-console.log(addressInput);
+
 
 request({
-    url: `${addressInput}`,
-    json: true
+    url:`http://www.mapquestapi.com/geocoding/v1/address?key=XenCxMaTYWNp2tUNaDeyMJdMBie4637W&location=${addressInput}`,
+    json:true
 }, (error, response, body) => {
-    //  console.log(JSON.stringify(body, undefined, 2));
+      if(error){
+         console.log('Unable to connect to map quest servers');
+      }else if(body.results[0].locations[0].adminArea5) {
+        console.log(`Address:   ${body.results[0].providedLocation.location}`);
+        console.log(`Latitude:  ${body.results[0].locations[0].latLng.lat}`);
+        console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`); 
+      }else{
+        console.log('Unable to find the address');
+      }
+    
 
-
-
-
-    console.log(`Adress: ${body.results[0].providedLocation.location}`);
-    console.log('----');
-    console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
-    console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
-});
+})
